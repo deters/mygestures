ab96eba5b081c5df3f31c2d27992a74037a487f0
Lucas Augusto Deters <lucasdeters@gmail.com>
2013-09-30 00:03:31 -0300

    "Initial blank Debian package template"

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(mygestures):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="mygestures" command="/usr/bin/mygestures"
---
+++ debian/mygestures.default.ex
@@ -0,0 +1,10 @@
+# Defaults for mygestures initscript
+# sourced by /etc/init.d/mygestures
+# installed at /etc/default/mygestures by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
