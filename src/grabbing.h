e6bf804e3502cbeb3b1cb337d48652b4d1f30823
Lucas Augusto Deters <lucasdeters@gmail.com>
2014-10-17 11:55:55 -0300

    Fixed copyright messages.

--- src/grabbing.h
+++ src/grabbing.h
@@ -1,9 +1,15 @@
 /*
- * grabbing.h
- *
- *  Created on: Aug 31, 2013
- *      Author: deters
- */
+  Copyright 2013, 2014 Lucas Augusto Deters
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.  */
 
 #ifndef GRABBING_H_
 #define GRABBING_H_
