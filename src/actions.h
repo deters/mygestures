17f2a1b29bfd046ec62cc9dd2d549deab422d048
git@github.com:deters/openbox.git <deters@users.noreply.github.com>
2026-05-22 15:11:41 -0300

    Fix compile error by using forward declared struct action_ in actions.h

--- src/actions.h
+++ src/actions.h
@@ -56,6 +56,7 @@ void action_maximize(Display *dpy, Window w);
 void action_restore(Display *dpy, Window w);
 void action_toggle_maximized(Display *dpy, Window w);
 void action_keypress(Display *dpy, char *data);
-void execute_wayland_action(Action *action);
+struct action_;
+void execute_wayland_action(struct action_ *action);
 
 #endif
