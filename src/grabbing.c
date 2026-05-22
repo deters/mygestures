82ed1f59c73349584cac93e118bfd260d84b8233
git@github.com:deters/openbox.git <deters@users.noreply.github.com>
2026-05-22 16:19:36 -0300

    Implement Wayland active window capture for Sway and Hyprland in pure C

--- src/grabbing.c
+++ src/grabbing.c
@@ -121,26 +121,30 @@ static Status fetch_window_title(Display *dpy, Window w, char **out_window_title
 {
 	int status;
 	XTextProperty text_prop;
-	char **list;
-	int num;
+	char **list = NULL;
+	int num = 0;
 
 	status = XGetWMName(dpy, w, &text_prop);
 	if (!status || !text_prop.value || !text_prop.nitems)
 	{
-		*out_window_title = "";
+		*out_window_title = strdup("");
+		return 1;
 	}
 	status = Xutf8TextPropertyToTextList(dpy, &text_prop, &list, &num);
 
 	if (status < Success || !num || !*list)
 	{
-		*out_window_title = "";
+		*out_window_title = strdup("");
 	}
 	else
 	{
 		*out_window_title = (char *)strdup(*list);
 	}
 	XFree(text_prop.value);
-	XFreeStringList(list);
+	if (list)
+	{
+		XFreeStringList(list);
+	}
 
 	return 1;
 }
@@ -156,15 +160,13 @@ static ActiveWindowInfo *get_active_window_info(Display *dpy, Window win)
 
 	if (!dpy || win == 0)
 	{
-		ans->class = "";
-		ans->title = "";
+		ans->class = strdup("");
+		ans->title = strdup("");
 		return ans;
 	}
 
-	int ret, val;
-
-	char *win_title;
-	ret = fetch_window_title(dpy, win, &win_title);
+	char *win_title = NULL;
+	fetch_window_title(dpy, win, &win_title);
 
 	char *win_class = NULL;
 
@@ -176,30 +178,363 @@ static ActiveWindowInfo *get_active_window_info(Display *dpy, Window win)
 	{
 
 		if (class_hints.res_class != NULL)
-			win_class = class_hints.res_class;
+			win_class = strdup(class_hints.res_class);
 
-		if (win_class == NULL)
+		if (class_hints.res_name != NULL)
+			XFree(class_hints.res_name);
+
+		if (class_hints.res_class != NULL)
+			XFree(class_hints.res_class);
+	}
+
+	ans->class = win_class ? win_class : strdup("");
+	ans->title = win_title ? win_title : strdup("");
+
+	return ans;
+}
+
+typedef struct JsonNode {
+	char *name;
+	char *app_id;
+	char *class;
+	int focused;
+} JsonNode;
+
+static void skip_space(const char **ptr)
+{
+	while (**ptr && (**ptr == ' ' || **ptr == '\t' || **ptr == '\r' || **ptr == '\n'))
+	{
+		(*ptr)++;
+	}
+}
+
+static char *parse_json_string(const char **ptr)
+{
+	if (**ptr != '"') return NULL;
+	(*ptr)++; // skip open quote
+	const char *start = *ptr;
+	while (**ptr && **ptr != '"')
+	{
+		if (**ptr == '\\' && *(*ptr + 1))
 		{
-			win_class = "";
+			(*ptr)++; // skip escape char
+		}
+		(*ptr)++;
+	}
+	size_t len = *ptr - start;
+	char *str = malloc(len + 1);
+	if (str)
+	{
+		memcpy(str, start, len);
+		str[len] = '\0';
+	}
+	if (**ptr == '"') (*ptr)++; // skip close quote
+	return str;
+}
+
+static void parse_json_val(const char **ptr, JsonNode *focused_node, JsonNode *current_node);
+
+static void parse_json_object(const char **ptr, JsonNode *focused_node, JsonNode *current_node)
+{
+	if (**ptr != '{') return;
+	(*ptr)++; // skip '{'
+
+	JsonNode local_node;
+	memset(&local_node, 0, sizeof(local_node));
+
+	while (**ptr)
+	{
+		skip_space(ptr);
+		if (**ptr == '}')
+		{
+			(*ptr)++; // skip '}'
+			break;
+		}
+
+		char *key = parse_json_string(ptr);
+		if (!key) break;
+
+		skip_space(ptr);
+		if (**ptr == ':') (*ptr)++; // skip ':'
+		skip_space(ptr);
+
+		if (strcmp(key, "focused") == 0)
+		{
+			if (strncmp(*ptr, "true", 4) == 0)
+			{
+				local_node.focused = 1;
+				*ptr += 4;
+			}
+			else if (strncmp(*ptr, "false", 5) == 0)
+			{
+				local_node.focused = 0;
+				*ptr += 5;
+			}
+			else
+			{
+				parse_json_val(ptr, focused_node, &local_node);
+			}
+		}
+		else if (strcmp(key, "name") == 0)
+		{
+			local_node.name = parse_json_string(ptr);
+			if (!local_node.name)
+			{
+				parse_json_val(ptr, focused_node, &local_node);
+			}
+		}
+		else if (strcmp(key, "app_id") == 0)
+		{
+			local_node.app_id = parse_json_string(ptr);
+			if (!local_node.app_id)
+			{
+				parse_json_val(ptr, focused_node, &local_node);
+			}
+		}
+		else if (strcmp(key, "window_properties") == 0)
+		{
+			JsonNode sub_node;
+			memset(&sub_node, 0, sizeof(sub_node));
+			parse_json_val(ptr, focused_node, &sub_node);
+			if (sub_node.class)
+			{
+				local_node.class = strdup(sub_node.class);
+			}
+			else if (sub_node.name)
+			{
+				local_node.class = strdup(sub_node.name);
+			}
+
+			if (sub_node.class) free(sub_node.class);
+			if (sub_node.name) free(sub_node.name);
+		}
+		else if (strcmp(key, "class") == 0)
+		{
+			local_node.class = parse_json_string(ptr);
+			if (!local_node.class)
+			{
+				parse_json_val(ptr, focused_node, &local_node);
+			}
+		}
+		else
+		{
+			parse_json_val(ptr, focused_node, &local_node);
+		}
+
+		free(key);
+
+		skip_space(ptr);
+		if (**ptr == ',')
+		{
+			(*ptr)++; // skip ','
 		}
 	}
 
-	if (win_class)
+	if (local_node.focused)
 	{
-		ans->class = win_class;
-	}
-	else
-	{
-		ans->class = "";
+		if (focused_node->name) free(focused_node->name);
+		if (focused_node->app_id) free(focused_node->app_id);
+		if (focused_node->class) free(focused_node->class);
+
+		focused_node->focused = 1;
+		focused_node->name = local_node.name ? strdup(local_node.name) : NULL;
+		focused_node->app_id = local_node.app_id ? strdup(local_node.app_id) : NULL;
+		focused_node->class = local_node.class ? strdup(local_node.class) : NULL;
 	}
 
-	if (win_title)
+	if (local_node.name) free(local_node.name);
+	if (local_node.app_id) free(local_node.app_id);
+	if (local_node.class) free(local_node.class);
+}
+
+static void parse_json_array(const char **ptr, JsonNode *focused_node, JsonNode *current_node)
+{
+	if (**ptr != '[') return;
+	(*ptr)++; // skip '['
+	while (**ptr)
 	{
-		ans->title = win_title;
+		skip_space(ptr);
+		if (**ptr == ']')
+		{
+			(*ptr)++; // skip ']'
+			break;
+		}
+		parse_json_val(ptr, focused_node, current_node);
+		skip_space(ptr);
+		if (**ptr == ',')
+		{
+			(*ptr)++; // skip ','
+		}
+	}
+}
+
+static void parse_json_val(const char **ptr, JsonNode *focused_node, JsonNode *current_node)
+{
+	skip_space(ptr);
+	if (**ptr == '{')
+	{
+		parse_json_object(ptr, focused_node, current_node);
+	}
+	else if (**ptr == '[')
+	{
+		parse_json_array(ptr, focused_node, current_node);
+	}
+	else if (**ptr == '"')
+	{
+		char *s = parse_json_string(ptr);
+		if (s) free(s);
 	}
 	else
 	{
-		ans->title = "";
+		while (**ptr && **ptr != ',' && **ptr != '}' && **ptr != ']' && **ptr != ' ' && **ptr != '\n' && **ptr != '\r' && **ptr != '\t')
+		{
+			(*ptr)++;
+		}
+	}
+}
+
+static char *read_all_from_pipe(FILE *fp, size_t *out_len)
+{
+	size_t capacity = 4096;
+	size_t len = 0;
+	char *buf = malloc(capacity);
+	if (!buf) return NULL;
+
+	char chunk[4096];
+	size_t n;
+	while ((n = fread(chunk, 1, sizeof(chunk), fp)) > 0)
+	{
+		if (len + n >= capacity)
+		{
+			capacity *= 2;
+			char *new_buf = realloc(buf, capacity);
+			if (!new_buf)
+			{
+				free(buf);
+				return NULL;
+			}
+			buf = new_buf;
+		}
+		memcpy(buf + len, chunk, n);
+		len += n;
+	}
+	buf[len] = '\0';
+	if (out_len) *out_len = len;
+	return buf;
+}
+
+static void parse_hyprctl_output(FILE *fp, char **out_class, char **out_title)
+{
+	char line[1024];
+	*out_class = NULL;
+	*out_title = NULL;
+	while (fgets(line, sizeof(line), fp))
+	{
+		char *p = line;
+		while (*p == ' ' || *p == '\t') p++;
+
+		size_t len = strlen(p);
+		while (len > 0 && (p[len - 1] == '\n' || p[len - 1] == '\r'))
+		{
+			p[len - 1] = '\0';
+			len--;
+		}
+
+		if (strncmp(p, "class: ", 7) == 0)
+		{
+			*out_class = strdup(p + 7);
+		}
+		else if (strncmp(p, "title: ", 7) == 0)
+		{
+			*out_title = strdup(p + 7);
+		}
+	}
+}
+
+static void free_active_window_info(ActiveWindowInfo *info)
+{
+	if (info)
+	{
+		if (info->title)
+		{
+			free(info->title);
+		}
+		if (info->class)
+		{
+			free(info->class);
+		}
+		free(info);
+	}
+}
+
+static ActiveWindowInfo *get_wayland_active_window_info(void)
+{
+	ActiveWindowInfo *ans = malloc(sizeof(ActiveWindowInfo));
+	bzero(ans, sizeof(ActiveWindowInfo));
+	ans->class = strdup("");
+	ans->title = strdup("");
+
+	char *swaysock = getenv("SWAYSOCK");
+	char *hyprland_sig = getenv("HYPRLAND_INSTANCE_SIGNATURE");
+
+	if (swaysock)
+	{
+		FILE *fp = popen("swaymsg -t get_tree 2>/dev/null", "r");
+		if (fp)
+		{
+			size_t len = 0;
+			char *json_str = read_all_from_pipe(fp, &len);
+			pclose(fp);
+			if (json_str)
+			{
+				JsonNode focused_node;
+				memset(&focused_node, 0, sizeof(focused_node));
+				const char *ptr = json_str;
+				parse_json_val(&ptr, &focused_node, &focused_node);
+
+				if (focused_node.focused)
+				{
+					char *class_val = focused_node.app_id ? focused_node.app_id : focused_node.class;
+					if (class_val)
+					{
+						free(ans->class);
+						ans->class = strdup(class_val);
+					}
+					if (focused_node.name)
+					{
+						free(ans->title);
+						ans->title = strdup(focused_node.name);
+					}
+				}
+
+				if (focused_node.name) free(focused_node.name);
+				if (focused_node.app_id) free(focused_node.app_id);
+				if (focused_node.class) free(focused_node.class);
+				free(json_str);
+			}
+		}
+	}
+	else if (hyprland_sig)
+	{
+		FILE *fp = popen("hyprctl activewindow 2>/dev/null", "r");
+		if (fp)
+		{
+			char *class_val = NULL;
+			char *title_val = NULL;
+			parse_hyprctl_output(fp, &class_val, &title_val);
+			pclose(fp);
+
+			if (class_val)
+			{
+				free(ans->class);
+				ans->class = class_val;
+			}
+			if (title_val)
+			{
+				free(ans->title);
+				ans->title = title_val;
+			}
+		}
 	}
 
 	return ans;
@@ -448,7 +783,7 @@ static void execute_action(Grabber *self, Action *action, Window focused_window)
 static void free_grabbed(Capture *free_me)
 {
 	assert(free_me);
-	free(free_me->active_window_info);
+	free_active_window_info(free_me->active_window_info);
 	free(free_me);
 }
 
@@ -766,8 +1101,28 @@ void grabbing_end_movement(Grabber *self, int new_x, int new_y,
 		expression_list[0] = self->fine_direction_sequence;
 		expression_list[1] = self->rought_direction_sequence;
 
-		ActiveWindowInfo *window_info = get_active_window_info(self->dpy,
-															   target_window);
+		ActiveWindowInfo *window_info = NULL;
+		if (getenv("SWAYSOCK") || getenv("HYPRLAND_INSTANCE_SIGNATURE"))
+		{
+			window_info = get_wayland_active_window_info();
+		}
+
+		if ((!window_info || (strlen(window_info->class) == 0 && strlen(window_info->title) == 0)) && self->dpy)
+		{
+			if (window_info)
+			{
+				free_active_window_info(window_info);
+			}
+			window_info = get_active_window_info(self->dpy, target_window);
+		}
+
+		if (!window_info)
+		{
+			window_info = malloc(sizeof(ActiveWindowInfo));
+			bzero(window_info, sizeof(ActiveWindowInfo));
+			window_info->class = strdup("");
+			window_info->title = strdup("");
+		}
 
 		grab = malloc(sizeof(Capture));
 
