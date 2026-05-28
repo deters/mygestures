bde9bbb377c0fe60b0669f320554aa0f4037a5e3
git@github.com:deters/openbox.git <deters@users.noreply.github.com>
2026-05-28 17:06:46 -0300

    feat: enhance gestos UI with sidebar, action details, and improved styling

--- src/gestos.c
+++ src/gestos.c
@@ -1,4 +1,5 @@
 #include <gtk/gtk.h>
+#include <string.h>
 #include "configuration.h"
 #include "mygestures.h"
 
@@ -6,17 +7,23 @@ typedef struct {
     GtkApplication *app;
     Configuration *config;
     GtkWidget *window;
-    GtkWidget *list_box;
+    GtkWidget *sidebar_list;
+    GtkWidget *main_list;
+    GtkWidget *search_entry;
+    GtkWidget *context_title;
+    Context *current_context;
 } GestosApp;
 
-static void add_gesture_row(GtkWidget *list_box, Gesture *gesture) {
+static void add_gesture_row(GestosApp *gestos, Gesture *gesture) {
     GtkWidget *row = gtk_list_box_row_new();
-    GtkWidget *box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 12);
-    gtk_widget_set_margin_start(box, 12);
-    gtk_widget_set_margin_end(box, 12);
-    gtk_widget_set_margin_top(box, 8);
-    gtk_widget_set_margin_bottom(box, 8);
+    GtkWidget *main_vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 4);
+    gtk_widget_set_margin_start(main_vbox, 16);
+    gtk_widget_set_margin_end(main_vbox, 16);
+    gtk_widget_set_margin_top(main_vbox, 12);
+    gtk_widget_set_margin_bottom(main_vbox, 12);
 
+    GtkWidget *top_hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 12);
+    
     GtkWidget *label_name = gtk_label_new(gesture->name);
     gtk_widget_set_halign(label_name, GTK_ALIGN_START);
     gtk_widget_set_hexpand(label_name, TRUE);
@@ -24,48 +31,76 @@ static void add_gesture_row(GtkWidget *list_box, Gesture *gesture) {
 
     GtkWidget *label_move = gtk_label_new(gesture->movement ? gesture->movement->name : "Unknown");
     gtk_widget_set_halign(label_move, GTK_ALIGN_END);
-    gtk_widget_add_css_class(label_move, "dim-label");
+    gtk_widget_add_css_class(label_move, "move-badge");
 
-    gtk_box_append(GTK_BOX(box), label_name);
-    gtk_box_append(GTK_BOX(box), label_move);
-    gtk_list_box_row_set_child(GTK_LIST_BOX_ROW(row), box);
+    gtk_box_append(GTK_BOX(top_hbox), label_name);
+    gtk_box_append(GTK_BOX(top_hbox), label_move);
+    gtk_box_append(GTK_BOX(main_vbox), top_hbox);
 
-    gtk_list_box_append(GTK_LIST_BOX(list_box), row);
+    if (gesture->action_count > 0 && gesture->action_list[0]->original_str) {
+        GtkWidget *label_action = gtk_label_new(gesture->action_list[0]->original_str);
+        gtk_widget_set_halign(label_action, GTK_ALIGN_START);
+        gtk_widget_add_css_class(label_action, "action-label");
+        gtk_label_set_ellipsize(GTK_LABEL(label_action), PANGO_ELLIPSIZE_END);
+        gtk_box_append(GTK_BOX(main_vbox), label_action);
+    }
+
+    gtk_list_box_row_set_child(GTK_LIST_BOX_ROW(row), main_vbox);
+    gtk_list_box_append(GTK_LIST_BOX(gestos->main_list), row);
+}
+
+static void refresh_gesture_list(GestosApp *gestos) {
+    /* Clear existing */
+    GtkWidget *child = gtk_widget_get_first_child(gestos->main_list);
+    while (child) {
+        GtkWidget *next = gtk_widget_get_next_sibling(child);
+        gtk_list_box_remove(GTK_LIST_BOX(gestos->main_list), child);
+        child = next;
+    }
+
+    if (!gestos->current_context) return;
+
+    gtk_label_set_text(GTK_LABEL(gestos->context_title), gestos->current_context->name);
+
+    const char *search_text = gtk_editable_get_text(GTK_EDITABLE(gestos->search_entry));
+
+    for (int i = 0; i < gestos->current_context->gesture_count; i++) {
+        Gesture *g = gestos->current_context->gesture_list[i];
+        if (search_text && strlen(search_text) > 0) {
+            if (!g_strrstr(g->name, search_text) && 
+                !(g->movement && g_strrstr(g->movement->name, search_text))) {
+                continue;
+            }
+        }
+        add_gesture_row(gestos, g);
+    }
+}
+
+static void on_sidebar_row_selected(GtkListBox *list, GtkListBoxRow *row, gpointer user_data) {
+    if (!row) return;
+    GestosApp *gestos = (GestosApp *)user_data;
+    int index = gtk_list_box_row_get_index(row);
+    if (index >= 0 && index < gestos->config->context_count) {
+        gestos->current_context = gestos->config->context_list[index];
+        refresh_gesture_list(gestos);
+    }
+}
+
+static void on_search_changed(GtkSearchEntry *entry, gpointer user_data) {
+    refresh_gesture_list((GestosApp *)user_data);
 }
 
 static void on_about_clicked(GtkWidget *widget, gpointer user_data) {
     GtkWindow *parent = GTK_WINDOW(user_data);
     gtk_show_about_dialog(parent,
         "program-name", "Gestos",
-        "version", "1.0.0",
+        "version", "1.1.0",
         "copyright", "Copyright © 2026",
-        "comments", "A modern UI for MyGestures",
+        "comments", "A modern, complete UI for MyGestures",
         "authors", (const char *[]){"Gemini CLI", NULL},
         "logo-icon-name", "input-mouse",
+        "license-type", GTK_LICENSE_GPL_3_0,
         NULL);
-}
-
-static void on_search_changed(GtkSearchEntry *entry, gpointer user_data) {
-    GestosApp *gestos = (GestosApp *)user_data;
-    const char *text = gtk_editable_get_text(GTK_EDITABLE(entry));
-    
-    GtkWidget *child = gtk_widget_get_first_child(gestos->list_box);
-    while (child) {
-        GtkWidget *row_box = gtk_list_box_row_get_child(GTK_LIST_BOX_ROW(child));
-        GtkWidget *label = gtk_widget_get_first_child(row_box); // First label is name
-        const char *label_text = gtk_label_get_text(GTK_LABEL(label));
-        
-        if (text && strlen(text) > 0) {
-            if (g_strrstr(label_text, text)) {
-                gtk_widget_set_visible(child, TRUE);
-            } else {
-                gtk_widget_set_visible(child, FALSE);
-            }
-        } else {
-            gtk_widget_set_visible(child, TRUE);
-        }
-        child = gtk_widget_get_next_sibling(child);
-    }
 }
 
 static void activate(GtkApplication *app, gpointer user_data) {
@@ -73,7 +108,7 @@ static void activate(GtkApplication *app, gpointer user_data) {
 
     gestos->window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(gestos->window), "Gestos");
-    gtk_window_set_default_size(GTK_WINDOW(gestos->window), 600, 450);
+    gtk_window_set_default_size(GTK_WINDOW(gestos->window), 850, 600);
 
     GtkWidget *header = gtk_header_bar_new();
     gtk_window_set_titlebar(GTK_WINDOW(gestos->window), header);
@@ -82,53 +117,105 @@ static void activate(GtkApplication *app, gpointer user_data) {
     gtk_header_bar_pack_end(GTK_HEADER_BAR(header), about_btn);
     g_signal_connect(about_btn, "clicked", G_CALLBACK(on_about_clicked), gestos->window);
 
-    GtkWidget *main_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
-    gtk_window_set_child(GTK_WINDOW(gestos->window), main_box);
+    GtkWidget *paned = gtk_paned_new(GTK_ORIENTATION_HORIZONTAL);
+    gtk_window_set_child(GTK_WINDOW(gestos->window), paned);
+    gtk_paned_set_wide_handle(GTK_PANED(paned), FALSE);
+    gtk_paned_set_position(GTK_PANED(paned), 200);
 
-    GtkWidget *search_bar_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
-    gtk_widget_set_margin_start(search_bar_box, 20);
-    gtk_widget_set_margin_end(search_bar_box, 20);
-    gtk_widget_set_margin_top(search_bar_box, 10);
-    gtk_box_append(GTK_BOX(main_box), search_bar_box);
+    /* Sidebar */
+    GtkWidget *sidebar_vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+    gtk_widget_add_css_class(sidebar_vbox, "sidebar");
+    gtk_paned_set_start_child(GTK_PANED(paned), sidebar_vbox);
 
-    GtkWidget *search_entry = gtk_search_entry_new();
-    gtk_widget_set_hexpand(search_entry, TRUE);
-    gtk_box_append(GTK_BOX(search_bar_box), search_entry);
-    g_signal_connect(search_entry, "search-changed", G_CALLBACK(on_search_changed), gestos);
+    GtkWidget *sidebar_title = gtk_label_new("Applications");
+    gtk_widget_set_halign(sidebar_title, GTK_ALIGN_START);
+    gtk_widget_set_margin_start(sidebar_title, 16);
+    gtk_widget_set_margin_top(sidebar_title, 16);
+    gtk_widget_set_margin_bottom(sidebar_title, 8);
+    gtk_widget_add_css_class(sidebar_title, "sidebar-header");
+    gtk_box_append(GTK_BOX(sidebar_vbox), sidebar_title);
+
+    gestos->sidebar_list = gtk_list_box_new();
+    gtk_widget_add_css_class(gestos->sidebar_list, "sidebar-list");
+    gtk_box_append(GTK_BOX(sidebar_vbox), gestos->sidebar_list);
+    g_signal_connect(gestos->sidebar_list, "row-selected", G_CALLBACK(on_sidebar_row_selected), gestos);
+
+    /* Main Content */
+    GtkWidget *content_vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+    gtk_widget_set_hexpand(content_vbox, TRUE);
+    gtk_paned_set_end_child(GTK_PANED(paned), content_vbox);
+
+    GtkWidget *content_header = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 12);
+    gtk_widget_set_margin_start(content_header, 30);
+    gtk_widget_set_margin_end(content_header, 30);
+    gtk_widget_set_margin_top(content_header, 20);
+    gtk_widget_set_margin_bottom(content_header, 10);
+    gtk_box_append(GTK_BOX(content_vbox), content_header);
+
+    gestos->context_title = gtk_label_new("Global");
+    gtk_widget_add_css_class(gestos->context_title, "context-title");
+    gtk_widget_set_hexpand(gestos->context_title, TRUE);
+    gtk_widget_set_halign(gestos->context_title, GTK_ALIGN_START);
+    gtk_box_append(GTK_BOX(content_header), gestos->context_title);
+
+    gestos->search_entry = gtk_search_entry_new();
+    gtk_widget_set_size_request(gestos->search_entry, 200, -1);
+    gtk_box_append(GTK_BOX(content_header), gestos->search_entry);
+    g_signal_connect(gestos->search_entry, "search-changed", G_CALLBACK(on_search_changed), gestos);
 
     GtkWidget *scrolled = gtk_scrolled_window_new();
     gtk_widget_set_vexpand(scrolled, TRUE);
-    gtk_box_append(GTK_BOX(main_box), scrolled);
+    gtk_box_append(GTK_BOX(content_vbox), scrolled);
 
-    gestos->list_box = gtk_list_box_new();
-    gtk_list_box_set_selection_mode(GTK_LIST_BOX(gestos->list_box), GTK_SELECTION_NONE);
-    gtk_widget_set_margin_start(gestos->list_box, 20);
-    gtk_widget_set_margin_end(gestos->list_box, 20);
-    gtk_widget_set_margin_top(gestos->list_box, 20);
-    gtk_widget_set_margin_bottom(gestos->list_box, 20);
-    gtk_widget_add_css_class(gestos->list_box, "boxed-list");
-    gtk_scrolled_window_set_child(GTK_SCROLLED_WINDOW(scrolled), gestos->list_box);
+    gestos->main_list = gtk_list_box_new();
+    gtk_list_box_set_selection_mode(GTK_LIST_BOX(gestos->main_list), GTK_SELECTION_NONE);
+    gtk_widget_set_margin_start(gestos->main_list, 30);
+    gtk_widget_set_margin_end(gestos->main_list, 30);
+    gtk_widget_set_margin_top(gestos->main_list, 10);
+    gtk_widget_set_margin_bottom(gestos->main_list, 30);
+    gtk_widget_add_css_class(gestos->main_list, "boxed-list");
+    gtk_scrolled_window_set_child(GTK_SCROLLED_WINDOW(scrolled), gestos->main_list);
 
     /* Load configuration */
     gestos->config = configuration_new();
     configuration_load_from_defaults(gestos->config, 0);
 
-    /* Populate list with global gestures */
-    if (gestos->config->context_count > 0) {
-        Context *global = gestos->config->context_list[0]; // Assuming first is global
-        for (int i = 0; i < global->gesture_count; i++) {
-            add_gesture_row(gestos->list_box, global->gesture_list[i]);
-        }
+    /* Populate sidebar */
+    for (int i = 0; i < gestos->config->context_count; i++) {
+        GtkWidget *row = gtk_list_box_row_new();
+        GtkWidget *label = gtk_label_new(gestos->config->context_list[i]->name);
+        gtk_widget_set_halign(label, GTK_ALIGN_START);
+        gtk_widget_set_margin_start(label, 16);
+        gtk_widget_set_margin_top(label, 8);
+        gtk_widget_set_margin_bottom(label, 8);
+        gtk_list_box_row_set_child(GTK_LIST_BOX_ROW(row), label);
+        gtk_list_box_append(GTK_LIST_BOX(gestos->sidebar_list), row);
     }
 
-    /* CSS for that Apple-like feel */
+    /* Initial selection */
+    if (gestos->config->context_count > 0) {
+        GtkListBoxRow *first = gtk_list_box_get_row_at_index(GTK_LIST_BOX(gestos->sidebar_list), 0);
+        gtk_list_box_select_row(GTK_LIST_BOX(gestos->sidebar_list), first);
+        gestos->current_context = gestos->config->context_list[0];
+        refresh_gesture_list(gestos);
+    }
+
+    /* Professional CSS Styling */
     GtkCssProvider *provider = gtk_css_provider_new();
     gtk_css_provider_load_from_data(provider,
-        ".boxed-list { border: 1px solid #ddd; border-radius: 8px; background: white; }\n"
-        ".boxed-list row { border-bottom: 1px solid #eee; }\n"
+        "window { background-color: #f5f5f7; }\n"
+        ".sidebar { background-color: #ebebeb; border-right: 1px solid #d1d1d1; }\n"
+        ".sidebar-header { color: #6e6e73; font-weight: bold; font-size: 0.8em; text-transform: uppercase; }\n"
+        ".sidebar-list { background: transparent; }\n"
+        ".sidebar-list row { border-radius: 6px; margin: 2px 8px; }\n"
+        ".sidebar-list row:selected { background-color: #007aff; color: white; }\n"
+        ".context-title { font-size: 1.8em; font-weight: 800; color: #1d1d1f; }\n"
+        ".boxed-list { border: 1px solid #d2d2d7; border-radius: 12px; background: white; }\n"
+        ".boxed-list row { border-bottom: 1px solid #f5f5f7; }\n"
         ".boxed-list row:last-child { border-bottom: none; }\n"
-        ".dim-label { color: #888; font-size: 0.9em; }\n"
-        "window { background-color: #f5f5f7; }\n", -1);
+        ".move-badge { background-color: #f2f2f7; color: #007aff; padding: 4px 10px; border-radius: 10px; font-weight: bold; font-size: 0.85em; }\n"
+        ".action-label { color: #86868b; font-size: 0.9em; }\n"
+        "scrollbar { background: transparent; }\n", -1);
     gtk_style_context_add_provider_for_display(gdk_display_get_default(),
                                                GTK_STYLE_PROVIDER(provider),
                                                GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
